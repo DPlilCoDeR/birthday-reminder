c8103da8acc6d086d59100cfc4bf2b39e6b8c4bf
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-07 17:12:46 +0100

    render list of users, add button with func for delete list and styles

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,24 +1,21 @@
 import './List.css'
-
-//components
-import { useState } from 'react';
+import { useState } from 'react'
 
 //Components
 import BirthdayUser from "../BirthdayUser/BirthdayUser";
-
-//hook
-import FetchData from '../../hooks/Fetchdata';
+import data from '../../data';
 
 
 const List = () => {
-
-    const {data, loading, error} = FetchData();
-    console.log(data)
+    const [users, setUsers] = useState(data)
 
     return (
-        <div className='List'>
-            {data.map(user => <BirthdayUser key={user.id} data={user}/> )}
-            
+        <div className='container'>
+            <h3>{users.length} Birthdays today</h3>
+            {users.map(user => 
+                <BirthdayUser key={user.id} user={user}/>
+            )}
+            <button onClick={() => setUsers([])}>Delete list</button>
         </div>
     )
 }
